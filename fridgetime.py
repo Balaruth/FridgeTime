34615c50b1ceb3c83b11ab3be3aea6d479471170
Grant Donoghue <grant.donoghue@gmail.com>
2019-02-08 17:19:23 +0100

    Version 0.2

--- fridgetime.py
+++ fridgetime.py
@@ -1,16 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+testdata = [
+    {
+        'user': 'Grant Donoghue',
+        'recipename': 'Omelette',
+        'ingredients': '3 Eggs'
+    }
+]
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "<h1>Homepage</h1>"
+    return render_template('index.html', testdata=testdata)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page </h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
