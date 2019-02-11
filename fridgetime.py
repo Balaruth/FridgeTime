1cce8d5a7205f104ffa8639d4503f4c8947e5446
Grant Donoghue <grant.donoghue@gmail.com>
2019-02-11 13:31:05 +0100

    Version 0.3

--- fridgetime.py
+++ fridgetime.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e64498bd6adeb194264c388d1aa84a46'
 
 testdata = [
     {
@@ -18,7 +21,27 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()  # passed from forms.py
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Register', form=form)  # passed from form variable
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'You have been logged in!', 'success')
+        return redirect(url_for('index'))
+    else:
+        flash(f'Login unsuccessful. Please check username and password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
