90aa67a8445b5f55b94b4cb4021dfc77f71f8247
Grant Donoghue <grant.donoghue@gmail.com>
2019-02-12 14:45:28 +0100

    Version 0.4

--- fridgetime.py
+++ fridgetime.py
@@ -1,8 +1,51 @@
+from datetime import datetime
 from flask import Flask, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e64498bd6adeb194264c388d1aa84a46'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    recipes = db.relationship('Recipe', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Recipe(db.Model):
+    __tablename__ = 'recipe'
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_name = db.Column(db.String(30), nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # table name in lower case
+    ingredients = db.relationship('Ingredient', backref='recipe', lazy=True)
+
+    def __repr__(self):
+        return f"Recipe('{self.recipe_name}', '{self.ingredients}')"
+
+
+class Ingredient(db.Model):
+    __tablename__ = 'ingredient'
+    id = db.Column(db.Integer, primary_key=True)
+    ingredient_name = db.Column(db.String(30))
+    quantity = db.Column(db.Integer)
+    type = db.Column(db.String)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
+
+
+    def __repr__(self):
+        return f"Recipe('{self.ingredient_name}', '{self.quantity}', '{self.type}')"
+
 
 testdata = [
     {
